5cbb7926d36c9afdcd3606a6fe864efe02219a17
luiss6 <44926678+luiss6@users.noreply.github.com>
2019-04-30 15:03:12 +0100

    Updates

---
+++ precache-manifest.cec82dbaca64a9750c07df36329de10c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de2aa0dd73f5d43d00d55f436aebe586",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "910f3159e418a5bd840b",
+    "url": "/portfolio/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/portfolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "910f3159e418a5bd840b",
+    "url": "/portfolio/static/js/main.55dc58d7.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
